3b604fc8edcb209bfd5eeee2be1ae39aae520e74
Serhii Myshko <sergeiown@gmail.com>
2023-04-12 20:15:34 +0300

    add moveBall() function

--- app.js
+++ app.js
@@ -8,3 +8,23 @@ let xSpeed = 5;
 let ySpeed = 5;
 let color = '#ff0000';
 let timeout = null;
+
+function moveBall() {
+    x += xSpeed;
+    y += ySpeed;
+
+    if (x + ball.clientWidth > screenWidth || x < 0) {
+        xSpeed = -xSpeed;
+    }
+
+    if (y + ball.clientHeight > screenHeight || y < 0) {
+        ySpeed = -ySpeed;
+    }
+
+    ball.style.left = x + 'px';
+    ball.style.top = y + 'px';
+    ball.style.backgroundColor = color;
+    ball.style.boxShadow = `0 0 10px 10px ${color}`;
+
+    requestAnimationFrame(moveBall);
+}
