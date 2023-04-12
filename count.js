3db2da1a0c239cf1109d3e3d7c79ed3fdd5e5064
Serhii Myshko <sergeiown@gmail.com>
2023-04-13 01:19:40 +0300

    make the information label shorter

--- count.js
+++ count.js
@@ -3,5 +3,5 @@ let caughtMiceCount = 0;
 
 export default function countCaughtMice() {
     caughtMiceCount++;
-    caughtCount.innerText = `Mice have already been caught: ${caughtMiceCount}`;
+    caughtCount.innerText = `Mice have been caught: ${caughtMiceCount}`;
 }
