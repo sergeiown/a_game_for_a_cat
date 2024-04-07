97a4e863ccbd08ce89b1013c8b24d05f869a3b6b
Serhii Myshko <sergeiown@gmail.com>
2024-04-07 22:46:19 +0300

    add cookie

--- count.js
+++ count.js
@@ -3,7 +3,20 @@
 const caughtCount = document.getElementById('count');
 let caughtMiceCount = 0;
 
+function loadCaughtMiceCount() {
+    const cookies = document.cookie.split(';');
+    for (let cookie of cookies) {
+        const [name, value] = cookie.trim().split('=');
+        if (name === 'caughtMiceCount') {
+            return parseInt(value) || 0;
+        }
+    }
+    return 0;
+}
+
 export default function countCaughtMice() {
+    caughtMiceCount = loadCaughtMiceCount();
     caughtMiceCount++;
+    document.cookie = `caughtMiceCount=${caughtMiceCount}; expires=Session`;
     caughtCount.innerText = `Mice have been caught: ${caughtMiceCount}`;
 }
